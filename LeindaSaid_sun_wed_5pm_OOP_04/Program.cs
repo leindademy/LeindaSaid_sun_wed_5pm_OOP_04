ed85c6365ce1da55591618f30573b04dd6ac1a03
Leinda Demy <demyleinda@gmail.com>
2024-07-17 01:29:13 +0300

    Initial commit

---
+++ LeindaSaid_sun_wed_5pm_OOP_04/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace LeindaSaid_sun_wed_5pm_OOP_04
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        
+        {
+            Maths operation = new Maths ();
+            ////operation.Add(3, 3);
+            ////operation.Subtract(3, 3);
+            ////operation.Multiply(3, 3);
+            ////operation.Divide(3, 3);
+
+            Maths.Add(3, 3);
+            Maths.Subtract(3, 3);
+            Maths.Multiply(3, 3);
+            Maths.Divide(3, 3);
+
+        }
+    }
+}
